62dcb1061cd08a1fd416470d02d8469f6a12bc76
StanleyY7 <stanleyyu7@gmail.com>
2024-07-20 22:19:49 +1000

    adding basic footer logic and styles

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,5 +1,25 @@
+import styles from "./footer.module.scss";
+
 function Footer() {
-  return <></>;
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.contactSectionContainer}></div>
+      <div className={styles.subFooterContainer}>
+        <p>We Accept: </p>
+        <div className={styles.paymentMethodContainer}>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+          <img></img>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
